c972f7ae6d96fa3dd8080fbaa7a1fe1e7855db36
imsp18 <shivangparmar537@gmail.com>
2024-01-20 23:47:34 +0530

    UI changes to shop section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div>
+        <div className="hero">
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,25 +6,25 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
-  const[menu, setMenu] = useState("shop")
+  const [menu, setMenu] = useState("shop")
 
   return (
     <div className="navbar">
-        <div className="nav-log">
-          <img src={logo} alt="logo" />
-          <p>SHOPPER</p>
-        </div>
-        <ul className="nav-menu">
-          <li onClick={()=>(setMenu("shop"))}><Link to="/">Shop</Link>{menu === "shop" ? <hr/>: <></>}</li>
-          <li onClick={()=>(setMenu("men"))}><Link to="/mens">Mens</Link>{menu === "men" ? <hr/>: <></>}</li>
-          <li onClick={()=>(setMenu("women"))}><Link to="/womens">Womens</Link>{menu === "women" ? <hr/>: <></>}</li>
-          <li onClick={()=>(setMenu("kids"))}><Link to="/kids">Kids</Link>{menu === "kids" ? <hr/>: <></>}</li>
-        </ul>
-        <div className="nav-login-cart">
-          <button>Login</button>
-          <img src={cart_icon} alt="cart" />
-          <div className="nav-cart-count">0</div>
-        </div>
+      <div className="nav-log">
+        <img src={logo} alt="logo" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={() => (setMenu("shop"))}><Link style={{ textDecoration: 'none'}} to="/">Shop</Link>{menu === "shop" ? <hr /> : <></>}</li>
+        <li onClick={() => (setMenu("men"))}><Link style={{ textDecoration: 'none'}} to="/mens">Mens</Link>{menu === "men" ? <hr /> : <></>}</li>
+        <li onClick={() => (setMenu("women"))}><Link style={{ textDecoration: 'none'}} to="/womens">Womens</Link>{menu === "women" ? <hr /> : <></>}</li>
+        <li onClick={() => (setMenu("kids"))}><Link style={{ textDecoration: 'none'}} to="/kids">Kids</Link>{menu === "kids" ? <hr /> : <></>}</li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link to="/login"><button>Login</button></Link>
+        <Link to="/cart"><img src={cart_icon} alt="cart" /></Link>
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
   )
 }
