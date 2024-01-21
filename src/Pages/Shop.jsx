b9a6427d2317212c5eb5da4bfa52d2e15ea3b7d6
imsp18 <shivangparmar537@gmail.com>
2024-01-21 23:14:19 +0530

    fixed ui error and made some changes to hero

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,31 +1,10 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
-import hand_icon from '../Components/Assets/hand_icon.png'
-import arrow_icon from '../Components/Assets/arrow.png'
-import hero_image from '../Components/Assets/hero_image.png'
 
 const Shop = () => {
   return (
     <div>
       <Hero />
-      <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
-        <div>
-          <div className="hand-hand-icon">
-            <p>new</p>
-            <img src={hand_icon} alt="" />
-          </div>
-          <p>collections</p>
-          <p>for everyone</p>
-        </div>
-      </div>
-      <div className="hero-latest-btn">
-        <div>Latest Collection</div>
-        <img src={arrow_icon} alt="" />
-      </div>
-      <div className="hero-right">
-        <img src={hero_image} alt="" />
-      </div>
     </div>
   )
 }
