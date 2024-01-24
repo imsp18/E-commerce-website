38c09cf38b258028d5ec4df7b1a091a9afe65db4
imsp18 <shivangparmar537@gmail.com>
2024-01-25 00:12:42 +0530

    ui changes to the popular, item section

--- src/Components/Items/Item.jsx
+++ src/Components/Items/Item.jsx
@@ -8,11 +8,11 @@ const Item = (props) => {
                 <img src={props.image} alt="" />
                 <p>{props.name}</p>
                 <div className="item-prices">
-                    <div className="items-prices-new">
-                        <p>{props.new_price}</p>
+                    <div className="items-price-new">
+                        ${props.new_price}
                     </div>
-                    <div className="items-prices-old">
-                        <p>{props.old_price}</p>
+                    <div className="items-price-old">
+                        ${props.old_price}
                     </div>
                 </div>
             </div>
